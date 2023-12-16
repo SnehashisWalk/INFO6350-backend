6a921c33d684913b8340b7faceaf5b2a8ea06ffd
Snehashis Lenka <slenka321@gmail.com>
2023-12-16 02:03:10 -0500

    Added route to get exercise based on type

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as exerciseController from "../controllers/exercise.controller.js";
+import Exercise from "../models/exercise/exercise.js";
 
 const router = express.Router();
 
@@ -10,5 +11,32 @@ router.get("/healthcheck", (req, res) => {
 router.post("/exercises", exerciseController.createExercise);
 router.get("/exercises", exerciseController.getExercises);
 router.delete('/exercises/:id', exerciseController.deleteExercise);
+
+router.get("/api/v1/exercises", async (req, res) => {
+    try {
+      let filter = {};
+      const { exerciseType, type } = req.query;
+  
+      if (exerciseType && type) {
+        filter = { exerciseType, type };
+      } else if (exerciseType) {
+        filter = { exerciseType };
+      } else if (type) {
+        filter = { type };
+      }
+  
+      const exerciseList = await Exercise.find(filter);
+  
+      if (exerciseList.length > 0) {
+        res.status(200).json({ exerciseList });
+      } else {
+        res.status(404).json({ message: 'No exercises found' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
 
 export default router;
